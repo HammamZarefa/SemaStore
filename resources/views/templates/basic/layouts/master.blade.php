87ada610c5a8ab0d44799226ed9b1d4e76a2e470
hammamZarefa <hammamzarefa@gmail.com>
2023-04-28 23:41:36 +0300

    order page ,back button

--- resources/views/templates/basic/layouts/master.blade.php
+++ resources/views/templates/basic/layouts/master.blade.php
@@ -67,6 +67,7 @@
             <div class="row align-items-center mb-30 justify-content-between">
                 <div class="col-lg-6 col-sm-6">
                     <h6 class="page-title">{{ __($page_title) }}</h6>
+                    <a href="{{ url()->previous() }}"><i class="fa fa-arrow-left"></i></a>
                 </div>
                 <div class="col-lg-6 col-sm-6 text-sm-right mt-sm-0 mt-3 right-part">
                     @stack('breadcrumb')
