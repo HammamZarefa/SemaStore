1523aafc940dcc4ce4c0bcc08f3f5d3177a4dd57
alaamhna3354 <76902248+alaamhna3354@users.noreply.github.com>
2024-02-18 14:06:10 -0500

    New design for home,login and register pages

--- resources/views/templates/basic/home.blade.php
+++ resources/views/templates/basic/home.blade.php
@@ -53,9 +53,24 @@
         <div class="carousel-inner">
             @foreach($banner as $key => $banner)
             <div class="carousel-item {{ $key == 0 ? 'active' : '' }}">
-                <img src="{{ getImage(imagePath()['banner']['path'].'/'. $banner->cover,imagePath()['banner']['size'])}}" class="d-block w-100" alt="...">
+                <img src="{{asset('assets/images/slider_bg.jpg')}}" class="d-block w-100" alt="...">
+                <div class="slider-info">
+                    <h1><span class="text-white" style="font-family: 'Oxanium', cursive !important;">Sema.</span><span style="color:#f45334;font-family: 'Oxanium', cursive !important;">Store</span></h1>
+                    <h3 class="text-white el-messiri"> الموقع الاول لشحن بطاقات الألعاب اونلاين</h3>
+                </div>
+                <img class="anime-img" src="{{asset('assets/images/slider_circle.png')}}" class="d-block w-100" alt="...">
+
             </div>
                 @endforeach
+                <div class="carousel-item">
+                <img src="{{asset('assets/images/about_bg.jpg')}}" class="d-block w-100" alt="...">
+                <div class="slider-info">
+                    <h1><span class="text-white" style="font-family: 'Oxanium', cursive !important;">Sema.</span><span style="color:#f45334;font-family: 'Oxanium', cursive !important;">Store</span></h1>
+                    <h3 class="text-white el-messiri"> الموقع الاول لشحن بطاقات الألعاب اونلاين</h3>
+                </div>
+                <img class="anime-img" src="{{asset('assets/images/slider_circle.png')}}" class="d-block w-100" alt="...">
+
+            </div>
         </div>
         <button class="carousel-control-prev" type="button" data-target="#carouselExampleControls" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -71,18 +86,33 @@
     <div class="products-contain">
         <div class="wrapper">
 
-            <h2><strong>@lang('Categories')</strong></h2>
+            <h2 class="mt-3">
+                <strong>@lang('Categories')</strong>
+                <br>
+                <img src="{{asset('assets/images/title_section.png')}}" alt="">
+            </h2>
 
-            <div class="cards">
-                @foreach($categories as $category)
+            <div class="cards-products row">
+                <!-- @foreach($categories as $category)
                     <figure class="card">
                         <img src="{{ getImage(imagePath()['category']['path'].'/'. $category->image,imagePath()['category']['size'])}}"/>
                         <figcaption>@lang($category->name)</figcaption>
                     </figure>
+                @endforeach -->
+                @foreach($categories as $category)
+                    <figure class="card-item col-lg-3 col-md-4 col-sm-6 col-6">
+                   <div class="image">
+                    <img src="{{asset('assets/images/category/62aa6fbc852701655336892.jpeg')}}" alt="">
+                    <!-- <img src="{{asset('assets/images/frontend/blog/5ff2e146346d21609752902.jpg')}}" alt=""> -->
+
+                   </div>
+                    <figcaption class="info">اسم المنتج</figcaption>
+                    </figure>
                 @endforeach
             </div>
-            <h2><strong>جديدنا</strong></h2>
-            <div class="news">
+           
+            
+            <!-- <div class="news">
                 <figure class="article">
 
                     <img src="https://mrreiha.keybase.pub/codepen/hover-fx/news1.jpg"/>
@@ -121,8 +151,105 @@
 
                 </figure>
 
-            </div>
+            </div> -->
 
+        </div>
+        <div style="background-color:#2a1d2e;padding:35px">
+            <h2 class="mt-3">
+                <strong>جديدنا</strong>
+                <br>
+                <img src="{{asset('assets/images/title_section.png')}}" alt="">
+            </h2>
+            <div class="row">
+                <div class="col-lg-4 col-md-6 mb-5">
+                    <div class="news-post mb-50">
+                        <div class="news-thumb mb-30">
+                            <a href="/">
+                                <img src="{{asset('assets/images/frontend/blog/5f9d068a341211604126346.jpg')}}" alt="">
+                            </a>
+                        </div>
+                        <div class="news-post-content">
+                            <h4>
+                                <a href="/"> الموقع الاول لشحن بطاقات الألعاب اونلاين </a>
+                            </h4>
+                            <div class="news-meta">
+                                <ul>
+                                    <li><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200zm61.8-104.4l-84.9-61.7c-3.1-2.3-4.9-5.9-4.9-9.7V116c0-6.6 5.4-12 12-12h32c6.6 0 12 5.4 12 12v141.7l66.8 48.6c5.4 3.9 6.5 11.4 2.6 16.8L334.6 349c-3.9 5.3-11.4 6.5-16.8 2.6z"/></svg>July 4, 2022</li>
+                                    <li><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M0 252.1V48C0 21.5 21.5 0 48 0h204.1a48 48 0 0 1 33.9 14.1l211.9 211.9c18.7 18.7 18.7 49.1 0 67.9L293.8 497.9c-18.7 18.7-49.1 18.7-67.9 0L14.1 286.1A48 48 0 0 1 0 252.1zM112 64c-26.5 0-48 21.5-48 48s21.5 48 48 48 48-21.5 48-48-21.5-48-48-48z"/></svg><a href="/">الموقع</a></li>
+                                </ul>
+                            </div>
+                            <p> الموقع الاول لشحن بطاقات الألعاب اونلاين 
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 mb-5">
+                    <div class="news-post mb-50">
+                        <div class="news-thumb mb-30">
+                            <a href="/">
+                                <img src="{{asset('assets/images/frontend/blog/5f99164f1baec1603868239.jpg')}}" alt="">
+                            </a>
+                        </div>
+                        <div class="news-post-content">
+                            <h4>
+                                <a href="/"> الموقع الاول لشحن بطاقات الألعاب اونلاين </a>
+                            </h4>
+                            <div class="news-meta">
+                                <ul>
+                                    <li><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200zm61.8-104.4l-84.9-61.7c-3.1-2.3-4.9-5.9-4.9-9.7V116c0-6.6 5.4-12 12-12h32c6.6 0 12 5.4 12 12v141.7l66.8 48.6c5.4 3.9 6.5 11.4 2.6 16.8L334.6 349c-3.9 5.3-11.4 6.5-16.8 2.6z"/></svg>July 4, 2022</li>
+                                    <li><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M0 252.1V48C0 21.5 21.5 0 48 0h204.1a48 48 0 0 1 33.9 14.1l211.9 211.9c18.7 18.7 18.7 49.1 0 67.9L293.8 497.9c-18.7 18.7-49.1 18.7-67.9 0L14.1 286.1A48 48 0 0 1 0 252.1zM112 64c-26.5 0-48 21.5-48 48s21.5 48 48 48 48-21.5 48-48-21.5-48-48-48z"/></svg><a href="/">الموقع</a></li>
+                                </ul>
+                            </div>
+                            <p> الموقع الاول لشحن بطاقات الألعاب اونلاين 
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 mb-5">
+                    <div class="news-post mb-50">
+                        <div class="news-thumb mb-30">
+                            <a href="/">
+                                <img src="{{asset('assets/images/frontend/blog/5f99164f1baec1603868239.jpg')}}" alt="">
+                            </a>
+                        </div>
+                        <div class="news-post-content">
+                            <h4>
+                                <a href="/"> الموقع الاول لشحن بطاقات الألعاب اونلاين </a>
+                            </h4>
+                            <div class="news-meta">
+                                <ul>
+                                    <li><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200zm61.8-104.4l-84.9-61.7c-3.1-2.3-4.9-5.9-4.9-9.7V116c0-6.6 5.4-12 12-12h32c6.6 0 12 5.4 12 12v141.7l66.8 48.6c5.4 3.9 6.5 11.4 2.6 16.8L334.6 349c-3.9 5.3-11.4 6.5-16.8 2.6z"/></svg>July 4, 2022</li>
+                                    <li><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M0 252.1V48C0 21.5 21.5 0 48 0h204.1a48 48 0 0 1 33.9 14.1l211.9 211.9c18.7 18.7 18.7 49.1 0 67.9L293.8 497.9c-18.7 18.7-49.1 18.7-67.9 0L14.1 286.1A48 48 0 0 1 0 252.1zM112 64c-26.5 0-48 21.5-48 48s21.5 48 48 48 48-21.5 48-48-21.5-48-48-48z"/></svg><a href="/">الموقع</a></li>
+                                </ul>
+                            </div>
+                            <p> الموقع الاول لشحن بطاقات الألعاب اونلاين 
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 mb-5">
+                    <div class="news-post mb-50">
+                        <div class="news-thumb mb-30">
+                            <a href="/">
+                                <img src="{{asset('assets/images/frontend/blog/60695237924ad1617515063.jpg')}}" alt="">
+                            </a>
+                        </div>
+                        <div class="news-post-content">
+                            <h4>
+                                <a href="/"> الموقع الاول لشحن بطاقات الألعاب اونلاين </a>
+                            </h4>
+                            <div class="news-meta">
+                                <ul>
+                                    <li><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200zm61.8-104.4l-84.9-61.7c-3.1-2.3-4.9-5.9-4.9-9.7V116c0-6.6 5.4-12 12-12h32c6.6 0 12 5.4 12 12v141.7l66.8 48.6c5.4 3.9 6.5 11.4 2.6 16.8L334.6 349c-3.9 5.3-11.4 6.5-16.8 2.6z"/></svg>July 4, 2022</li>
+                                    <li><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M0 252.1V48C0 21.5 21.5 0 48 0h204.1a48 48 0 0 1 33.9 14.1l211.9 211.9c18.7 18.7 18.7 49.1 0 67.9L293.8 497.9c-18.7 18.7-49.1 18.7-67.9 0L14.1 286.1A48 48 0 0 1 0 252.1zM112 64c-26.5 0-48 21.5-48 48s21.5 48 48 48 48-21.5 48-48-21.5-48-48-48z"/></svg><a href="/">الموقع</a></li>
+                                </ul>
+                            </div>
+                            <p> الموقع الاول لشحن بطاقات الألعاب اونلاين 
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                      </div>
         </div>
 
     </div>
--- resources/views/templates/basic/user/auth/login.blade.php
+++ resources/views/templates/basic/user/auth/login.blade.php
@@ -2,70 +2,55 @@
 
 @section('content')
     <!-- register-section start -->
-    <section class="register-section" style="background-image: url('{{asset($activeTemplateTrue.'images/bg.jpg')}}')">
+    <section class="login-section" style="background-image: url('{{asset('assets/images/login.png')}}')">
         <div class="container">
-            <div class="">
-                <div class="">
-                    
-                            <div class="sign-card">
-                               
                                 <form class="create-account-form register-form" method="POST" action="{{ route('user.login')}}"
                                       onsubmit="return submitUserForm();">
                                     @csrf
                                    
                                     <div class="row ">
-                                   
-                                        <div class="col-6 right-form">
-                                        <h4 class="title mt-4 mr-2 m-2">@lang('Login your account')</h4>
-                                        <div class="  mr-2 m-2">
+                                        <div class="col-md-6 col-12">
+                                        <h2 class="title">@lang('Login your account')</h2>
+                                        <div class="mb-3 input-item">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
                                             <input type="text" name="username" value="{{ old('username') }}" placeholder="@lang('Username or Email')" required>
                                         </div>
-                                        <div class="mr-2 m-2">
+                                        <div class="mb-3 input-item">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z"/></svg>
                                             <input id="password" type="password" name="password" required autocomplete="current-password" placeholder="@lang('Password')">
                                         </div>
 
-                                        <div class="mr-2 m-2">
+                                        <div class="m-2">
                                             @php echo loadReCaptcha() @endphp
                                         </div>
                                         @include($activeTemplate.'partials.custom-captcha')
-                                            <div class="d-flex align-items-center text-white flex-wrap mr-2 m-2 rem">
+                                            <div class="submit-contain text-white flex-wrap m-2 rem">
                                                 <div class="checkbox-item d-flex ">
                                                     <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                                                    <label for="remember">@lang('Remember Me')</label>
+                                                    <label for="remember" class="mx-2 text-white">@lang('Remember Me')</label>
                                                 </div>
-
-                                                <a class="text-anim" href="{{route('user.password.request')}}">@lang('Forgot Password?')</a>
+                                                        
+                                               
                                             </div>
+                                        <div class="m-2 submit-contain ">
+                                            <a class="mx-5 mb-3 text-white" href="{{route('user.password.request')}}">@lang('Forgot Password?')</a>
+                                            <button type="submit" class="btn-auth">@lang('Signin Now')</button>
+                                        </div>
+                                        </div>
+                                        <div class="col-1 Or d-md-block d-none">
 
-                                        <div class="mr-2 m-2">
-                                            <button type="submit" class="">@lang('Signin Now')</button>
                                         </div>
-                                        <div class="mr-2 m-2">
-                                <h4 class="title text-anim">
-                                    @lang('New here?')
-                                </h4>
-                                <a  class="text-anim" href="{{ route('user.register') }}" class="">@lang('Create Account')</a>
-                            </div>
+                                        <div class="col-md-5 col-12">
+                                        <h2 class="title">
+                                            @lang('New here?')
+                                        </h2>
+                                        <div class="m-2 submit-contain ">
+                                        <a class="btn-auth" href="{{ route('user.register') }}" class="">@lang('Create Account')</a>
                                         </div>
-                                        <div class="col-6 left-wall">
-                                        <img src="{{asset($activeTemplateTrue.'images/loginbg.png')}}" alt="">
+                                        
                                         </div>
                                     </div>
                                 </form>
-                            </div>
-                            
-                       
-                        <div class="col-lg-3 p-0"></div>
-                        {{--<div class="col-lg-6 p-0">--}}
-                            {{--<div class="change-catagory-area">--}}
-                                {{--<h4 class="title">--}}
-                                    {{--@lang('New here?')--}}
-                                {{--</h4>--}}
-                                {{--<a href="{{ route('user.register') }}" class="cmn-btn-active account-control-button">@lang('Create Account')</a>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-                </div>
-            </div>
         </div>
     </section>
     <!-- register-section end -->
